8c099a8284225f0385144cdce4a9d01c4b7780b9
Daniel da Rocha Fróes <danielfroes15@gmail.com>
2024-01-02 12:21:35 -0300

    Created player  animations

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace CuteNewtTest.MapGeneration
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] float _moveSpeed = 5f;
+        [SerializeField] Animator _animator;
+        [SerializeField] SpriteRenderer _renderer;
+
+
+
+        void Update()
+        {
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            float verticalInput = Input.GetAxisRaw("Vertical");
+
+            Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0f).normalized;
+            transform.position += moveDirection * _moveSpeed * Time.deltaTime;
+
+            _animator.SetFloat("HorizontalVelocity", Mathf.Abs(moveDirection.x));
+            _animator.SetFloat("VerticalVelocity", moveDirection.y);
+
+            _renderer.flipX = moveDirection.x < 0;
+
+
+        }
+    }
+}
